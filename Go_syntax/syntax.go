423226d4480b13eb38247489cd06f398d497a0f4
MICHAELKITH <michaelkithinji27@gmail.com>
2024-05-16 08:50:38 +0300

    Golang syntax

---
+++ Go_syntax/syntax.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var (
+	name  string
+	phone int
+)
+
+func main() {
+	name = "Michael"
+	phone = 221125585
+
+	fmt.Printf(" my name %v, and phone is %v \n", name, phone)
+}

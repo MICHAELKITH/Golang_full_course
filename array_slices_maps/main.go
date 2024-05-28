3a17335a0340f5a5de776a7104da1cc7de1f16ed
MICHAELKITH <michaelkithinji27@gmail.com>
2024-05-28 22:08:28 +0300

    Add simple for loop

--- array_slices_maps/main.go
+++ array_slices_maps/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("welcome to array, slices and maps using golang")
+
+	names := []string{"mike", "joan", "bro"}
+	for _, n := range names{
+		fmt.Printf("name are %v\n", n)
+	}
+
 }

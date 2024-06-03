cd150c9d12f1cb90f1ae2ecf8175886740385ef3
MICHAELKITH <michaelkithinji27@gmail.com>
2024-06-04 00:44:57 +0300

    Initialize pointer module and update main

--- pointers/main.go
+++ pointers/main.go
@@ -1,32 +1,30 @@
+// pointers/main.go
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/MICHAELKITH/Golang_full_course/pointer"
+)
 
 func main() {
-
-	n := 1000
-
-	fmt.Printf("Fibonacci(%d) is = %d \n", n, fibonacci(n) )
-
-	//0, 1, 2, 3, 5, 8, 13, 21 ......
+    n := 10
+    fmt.Printf("Fibonacci(%d) is = %d \n", n, fibonacci(n))
+    pointer.PrintMessage()
+    // 0, 1, 2, 3, 5, 8, 13, 21 ......
 }
 
 func fibonacci(n int) int {
-	if n == 0 {
-		return 0
-	} else if n == 1 {
-		return 1
-	}
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    }
 
-	//100
-	a, b  := 0, 1
+    a, b := 0, 1
 
-	for i:=2; i<=n; i++{
+    for i := 2; i <= n; i++ {
+        a, b = b, a+b // Update a and b without redeclaring them
+    }
 
-		a, b = b, a+b
-
-	}
-
-	return b
-
+    return b
 }

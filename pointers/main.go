3e087369510fdcdb9bf76010d02257a6d5a51165
MICHAELKITH <michaelkithinji27@gmail.com>
2024-06-03 23:54:06 +0300

    Add fibonacci

--- pointers/main.go
+++ pointers/main.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	n := 1000
+
+	fmt.Printf("Fibonacci(%d) is = %d \n", n, fibonacci(n) )
+
+	//0, 1, 2, 3, 5, 8, 13, 21 ......
+}
+
+func fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+
+	//100
+	a, b  := 0, 1
+
+	for i:=2; i<=n; i++{
+
+		a, b = b, a+b
+
+	}
+
+	return b
+
+}

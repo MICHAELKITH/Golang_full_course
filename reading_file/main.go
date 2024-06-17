1d1e8b021e49add1b195dc0b28d5714a651b10a0
MICHAELKITH <michaelkithinji27@gmail.com>
2024-06-17 23:18:42 +0300

    read and write in golang

--- reading_file/main.go
+++ reading_file/main.go
@@ -1,2 +1,48 @@
-package main  
+package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	// Write data into a file
+	fmt.Println("Writing data into a file")
+	err := writeFile("Welcome to Go!")
+	if err != nil {
+		log.Fatalf("Failed to write file: %v", err)
+	}
+
+	// Read data from the file
+	fmt.Println("Reading data from the file")
+	content, err := readFile()
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	fmt.Println("File content:")
+	fmt.Println(content)
+}
+
+func writeFile(message string) error {
+	file, err := os.Create("testgo.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(message)
+	if err != nil {
+		return err
+	}
+	fmt.Println("File created successfully")
+	return nil
+}
+
+func readFile() (string, error) {
+	bytes, err := os.ReadFile("testgo.txt")
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}

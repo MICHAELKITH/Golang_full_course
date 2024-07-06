53496ce44a1eb5d077c1b7ea176912cfe9864bd5
MICHAELKITH <michaelkithinji27@gmail.com>
2024-07-06 17:12:16 +0300

    WaitGroup

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,13 +1,30 @@
+// WaitGroups
 package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func main() {
-	go fmt.Println("Hello goroutines")
 
-	time.Sleep(time.Millisecond *2)
-	fmt.Println("Hello goroutines 2")
+func main ()  {
+	var wg  sync.WaitGroup
+
+	for i:=1; i <=5; i++{
+		wg.Add(1)
+
+		go func (){
+			defer wg.Done()
+
+			student(i)
+		}()
+	}
+	wg.Wait()
+}
+
+func student(id int){
+
+	fmt.Printf("Student %d starting\n",  id)
+
+
 }

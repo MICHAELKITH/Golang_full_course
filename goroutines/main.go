0446ef874d98f5c6c8af6670c701adbe6d95533c
MICHAELKITH <michaelkithinji27@gmail.com>
2024-07-25 23:25:05 +0300

    Add mutex

--- goroutines/main.go
+++ goroutines/main.go
@@ -8,13 +8,14 @@ import (
 func main() {
 
 	var wg sync.WaitGroup
+	var m sync.Mutex
 	//Add method
 
 	wg.Add(3)
 	fmt.Println("Starting")
-	go count("Runner 1",  &wg)
-	go count("Runner 2",  &wg)
-	go count("Runner 3",  &wg)
+	go count("Runner 1",  &wg, &m)
+	go count("Runner 2",  &wg, &m)
+	go count("Runner 3",  &wg, &m)
 
 	//sleep our program
 	wg.Wait()
@@ -22,12 +23,14 @@ func main() {
 	fmt.Print("Stopped Running")
 }
 
-func count(value string, wg *sync.WaitGroup){
+func count(value string, wg *sync.WaitGroup, m *sync.Mutex){
 	fmt.Println(value , "STARTED")
 
 	defer wg.Done()
 
 	for i:=1; i<=10; i++{
+		m.Lock()
 		fmt.Println(value, i)
+		m.Unlock()
 	}
 }
